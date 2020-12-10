0d991fcca86c7d730c010d6d68026f678519f17b
Lucas <laksteelhouse@gmail.com>
2020-12-10 11:51:09 +0100

    Pushing full fizz buzz program

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,7 +1,20 @@
+def has_zero_remainder?(number,divider)
+    number % divider == 0
+end
+
+
 def fizz_buzz(number)
-    if number % 3 == 0
-        return "fizz"
-    else 
-        return number
+    if has_zero_remainder?((number*10),10)
+        if has_zero_remainder?(number,5) && has_zero_remainder?(number,3)
+            return "fizz buzz"
+        elsif has_zero_remainder?(number,5)
+            return "buzz"
+        elsif has_zero_remainder?(number,3)
+            return "fizz"
+        else 
+            return number
+        end
+    else   
+        puts "That's not the way we play this game!"
     end
 end
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -1,11 +1,20 @@
 require './lib/fizz_buzz'
 
 describe 'fizz_buzz' do
+    it 'checks whether number is a whole number' do
+        expect(fizz_buzz('Dinosaur')).to eq false || nil
+    end
+
     it 'returns 1 if number is 1' do
         expect(fizz_buzz(1)).to eq 1 
     end
     it "returns 'fizz' if number is divisible by 3" do
         expect(fizz_buzz(3)).to eq 'fizz'
-      end
-
+    end
+    it "returns 'buzz' if number is divisible by 5" do
+        expect(fizz_buzz(5)).to eq 'buzz' 
+    end
+    it "returns 'fizz buss' if number is dibisible by both 3 and 5" do
+        expect(fizz_buzz(15)).to eq 'fizz buzz'
+    end
 end
